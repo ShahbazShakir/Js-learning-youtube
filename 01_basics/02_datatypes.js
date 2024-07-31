b52482e39d9b7c5b1930c5c5346fc529e2f95560
Shahbaz Shakir <mshahbazshakir251@gmail.com>
2024-07-31 15:15:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer Verson
+// alert( 3 + 3 )  we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3 ) // Code readability should be high
+
+
+let name = "Shahbaz"
+let age = 24
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => value not defined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
