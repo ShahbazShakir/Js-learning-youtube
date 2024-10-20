f3b4a4c3803695a65e777b7c53b9a580ae80836f
Shahbaz Shakir <mshahbazshakir251@gmail.com>
2024-10-20 18:26:34 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "shahbaz"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumer = Number(score)
+
+// console.log(typeof valueInNumer)
+// console.log(valueInNumer)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "shahbaz"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true , 0 => false
+// "" => false
+// "shahbaz" => true
+
+let isNumber = 33
+let stringNumber = String(isNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
